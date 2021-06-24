e60ba77bdcf263ce0c45212f69b3c2fe8bd7fded
Reugito <raosahebdhotreedu@gmail.com>
2021-06-24 22:12:07 +0530

    Java Day 5 Function Programs

--- Java_Day5_FunctionPrograms/src/com/bridglabz/programs/SumOf3IntAdds2Zero.java
+++ Java_Day5_FunctionPrograms/src/com/bridglabz/programs/SumOf3IntAdds2Zero.java
@@ -6,7 +6,7 @@ public class SumOf3IntAdds2Zero {
 
 	public static void main(String[] args) {
 		Utility utility = new Utility();
-		System.out.println("Enter the length of array n : ");
+		System.out.print("Enter the length of array n : ");
 		int n = utility.getIntValue();
 		int[] array = utility.getArray(n);
 		utility.getSumAddZero(array, n);
