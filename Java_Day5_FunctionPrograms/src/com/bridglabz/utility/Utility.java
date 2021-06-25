e2eb76e2c0cc42d363a5c1ee5e25e2777b3d9610
Reugito <raosahebdhotreedu@gmail.com>
2021-06-25 20:13:40 +0530

    added comments in utility class

--- Java_Day5_FunctionPrograms/src/com/bridglabz/utility/Utility.java
+++ Java_Day5_FunctionPrograms/src/com/bridglabz/utility/Utility.java
@@ -14,7 +14,7 @@ public class Utility {
 	public int getIntValue() {
 		return scanner.nextInt();
 	}
-	
+	// method to get 2d integer array from user
 	public Integer[][] getInt2DArray(int m, int n) {
 		Integer[][] array = new Integer[m][n];
 		System.out.println("Enter "+(m*n)+"integer values in intArray");
@@ -26,7 +26,7 @@ public class Utility {
 		}
 		return array;
 	}
-	
+	// method to get 2d double array from user
 	public Double[][] getDouble2DArray(int m, int n) {
 		Double[][] array = new Double[m][n];
 		System.out.println("Enter "+(m*n)+" double values in Doublearray");
@@ -38,7 +38,7 @@ public class Utility {
 		}
 		return array;
 	}
-	
+	// method to get 2d boolean array from user
 	public Boolean[][] getBoolean2DArray(int m, int n) {
 		Boolean[][] array = new Boolean[m][n];
 		System.out.println("Enter "+(m*n)+" boolean values in Boolean array");
@@ -50,7 +50,7 @@ public class Utility {
 		}
 		return array;
 	}
-	
+	// method to print all types of arrays
 	public <t>void printArray(t[][] Array, int m, int n) {
 		PrintWriter writer = new PrintWriter(System.out);
 		for (int i=0; i<m; i++){
@@ -61,7 +61,7 @@ public class Utility {
 			System.out.println();
 		}  
 	}
-	
+	// method to get array from user
 	public int[] getArray(int n) {
 		int[] array = new int[n];
 		System.out.println("Enter "+n+" elements in intArray");
@@ -70,7 +70,7 @@ public class Utility {
 		}
 		return array;	
 	}
-	
+	// method to print distinct tripplets whose sum is zero
 	public void getSumAddZero(int[] array, int n) {
 		for(int i=0;i<n;i++) {
 			for(int j=i+1; j<n; j++) {
@@ -84,8 +84,11 @@ public class Utility {
 		
 		System.out.println("number of uniq tripplet = "+cnt);
 	}
+	
 	static String tripletCollection = "";
 	static int cnt = 0;
+	
+	// method to select distinct tripplets
 	public void distinctTripplet(int num1, int num2, int num3) {
 		String str = ""+num1+num2+num3;
 		if(! tripletCollection.contains(str)){
@@ -94,13 +97,13 @@ public class Utility {
 			System.out.println(num1+", "+num2+", "+num3);
 		}
 	}
-	
+	// method to calculate euclidian distance
 	public void getEuclidianDist(int x, int y) {
 		double distance;
 		distance = Math.sqrt(Math.pow(x, 2)+Math.pow(y, 2));
 		System.out.println("Euclidian distance from points = "+distance);
 	}
-
+	// method to calculate quadratic roots
 	public void getRoots(int a, int b, int c) {
 		double root1, root2, delta;
 		delta = b*b - 4*a*c;
@@ -121,11 +124,11 @@ public class Utility {
 		      System.out.println("root1 = "+real+"-"+imaginary);
 		    }
 	}
-
+	// method to get double value as input
 	public double getDoubleValue() {
 		return scanner.nextDouble();
 	}
-
+	// method to get wind chill
 	public void getWindChill(double t, double v) {
 		if (t < 50) {
 			if(v >= 3 && v <= 120) {
